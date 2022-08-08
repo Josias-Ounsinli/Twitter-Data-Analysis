8e437c51a338999189301166993660c78bb994f0
Josias-Ounsinli <josias.ouns.djossou@gmail.com>
2022-08-08 22:48:40 +0100

    Modified extraction script

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -72,7 +72,7 @@ class TweetDfExtractor:
         try:
             is_sensitive = [x['possibly_sensitive'] for x in self.tweets_list]
         except KeyError:
-            is_sensitive = None
+            is_sensitive = []
 
         return is_sensitive
 
@@ -136,9 +136,9 @@ class TweetDfExtractor:
                 
 if __name__ == "__main__":
     # required column to be generated you should be creative and add more features
-    columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
+    columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count',
     'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
-    _, tweet_list = read_json("/home/jds98/10 Academy/Pycharm/africa_twitter_data.json")
+    _, tweet_list = read_json("/home/jds98/10 Academy/Pycharm/global_twitter_data.json")
     tweet = TweetDfExtractor(tweet_list)
     tweet_df = tweet.get_tweet_df() 
 
