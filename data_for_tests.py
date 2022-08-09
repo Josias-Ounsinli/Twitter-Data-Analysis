3be7a4dca768e2e67bb7e303462420857ddae823
Josias-Ounsinli <josias.ouns.djossou@gmail.com>
2022-08-09 22:40:03 +0100

    Modified

--- data_for_tests.py
+++ data_for_tests.py
@@ -1,20 +1,27 @@
-from extract_dataframe import *
+import json
+from textblob import TextBlob
 
-if __name__ == "__main__":
-    # required column to be generated you should be creative and add more features
-    columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count',
-    'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries','statuses_count']
-    _, tweet_list = read_json("/home/jds98/10 Academy/Pycharm/global_twitter_data.json")
-    tweet = TweetDfExtractor(tweet_list)
-    tweet_df = tweet.get_tweet_df()
+data = []
+for t in open("/home/jds98/10 Academy/Pycharm/africa_twitter_data.json",'r'):
+    data.append(json.loads(t))
 
-true_statuses_count = list(true_tweet_df['statuses_count'][:5])
-true_full_text = list(tweet_df['original_text'][:5])
-true_polarity = list(tweet_df['polarity'][:5])
-true_subjectivity = list(tweet_df['subjectivity'][:5])
-true_screen_name = list(tweet_df['original_author'][:5])
-true_followers_count = list(tweet_df['followers_count'][:5])
-true_friends_count = list(tweet_df['friends_count'][:5])
-true_sensitivity = list(tweet_df['possibly_sensitive'][:5])
-true_hashtags = list(tweet_df['hashtags'][:5])
-true_mentions = list(tweet_df['user_mentions'][:5])
+true_statuses_count = [x['user']['statuses_count'] for x in data[:5]]
+true_full_text = [x['full_text'] for x in data[:5]]
+true_polarity = [TextBlob(x).sentiment.polarity for x in true_full_text]
+true_subjectivity = [TextBlob(x).sentiment.subjectivity for x in true_full_text]
+true_screen_name = [x['user']['screen_name'] for x in data[:5]]
+true_followers_count = [x['user']['followers_count'] for x in data[:5]]
+true_friends_count = [x['user']['friends_count'] for x in data[:5]]
+true_sensitivity = []
+for x in data[:5]:
+    try:
+        true_sensitivity.append(x['retweeted_status']['possibly_sensitive'])
+    except KeyError:
+        try:
+            true_sensitivity.append(x["possibly_sensitive"])
+        except KeyError:
+            true_sensitivity.append(None)
+true_hashtags = [x['entities']['hashtags'] for x in data[:5]]
+true_mentions = [x['entities']['user_mentions'] for x in data[:5]]
+
+Z
